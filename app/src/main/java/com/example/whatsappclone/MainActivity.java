655c00f13cca1c2ed386172ecb130ca2b27150a6
Gerardo Juarez <gerardojuarez@Gerardos-MacBook-Air.local>
2022-10-29 23:26:14 -0400

    Update gradle

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -2,13 +2,82 @@ package com.example.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = "MainActivity";
+    private Button btnRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        initViews();
+        setClickLister();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            // Reload Activity
+            finish();
+            startActivity(getIntent());
+        } else {
+            Log.d(TAG, "No user");
+        }
+    }
+
+    private void initViews() {
+        btnRegister = findViewById(R.id.btn_register);
+    }
+
+    private void setClickLister() {
+        btnRegister.setOnClickListener(this);
+    }
+
+    private void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        assert user != null;
+                        Toast.makeText(MainActivity.this, user.getEmail(), Toast.LENGTH_SHORT).show();
+//                            updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                    }
+                });
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_register:
+                registerUser("test@example.com", "password");
+                break;
+            default:
+                Log.d(TAG, "Unknown button clicked");
+        }
     }
 }
