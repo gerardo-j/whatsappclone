e5a56e79bb3254d7d36f25a807edc8075311d066
Gerardo <gerardojuarez695@gmail.com>
2022-11-01 12:46:11 -0400

    Change mainactivity to include signin button

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,34 +1,28 @@
 package com.example.whatsappclone;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Button btnSignOut;
+    private static final List<AuthUI.IdpConfig> providers =
+            Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+    private Button btnSignIn;
     private FirebaseAuth mAuth;
 
     @Override
@@ -37,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        btnSignOut = findViewById(R.id.btn_signOut);
+        btnSignIn = findViewById(R.id.btn_sign_in);
         addClickListener();
     }
 
@@ -50,20 +44,20 @@ public class MainActivity extends AppCompatActivity {
             // Reload Activity
         } else {
             Log.d(TAG, "No user");
-            List<AuthUI.IdpConfig> providers = Collections.singletonList(
-                    new AuthUI.IdpConfig.EmailBuilder().build());
-
-            // Create and launch sign-in intent
-            Intent signInIntent = AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setAvailableProviders(providers)
-                    .build();
-            signInLauncher.launch(signInIntent);
         }
     }
 
     private void addClickListener() {
-        btnSignOut.setOnClickListener(view -> signOut());
+        btnSignIn.setOnClickListener(view -> signIn());
+    }
+
+    private void signIn() {
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
     }
 
     private void signOut() {
