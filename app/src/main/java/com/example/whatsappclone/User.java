1687762df3e9b54fa7583ccad91b287ebe9b7670
Gerardo <gerardojuarez695@gmail.com>
2022-11-13 20:10:26 -0500

    Updating User

--- app/src/main/java/com/example/whatsappclone/User.java
+++ app/src/main/java/com/example/whatsappclone/User.java
@@ -1,16 +1,34 @@
 package com.example.whatsappclone;
 
+import com.example.whatsappclone.Utils.MessageChannel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class User {
 
-    private String firstName, lastName, email;
+    private String id, firstName, lastName, email;
+    ArrayList<MessageChannel> channels;
 
     public User() {}
 
-    public User(String firstName, String lastName, String email) {
+    public User(String id, String firstName, String lastName, String email) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
+
+    public User(String id, String firstName, String lastName, String email, ArrayList<MessageChannel> channels) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.channels = channels;
+    }
+
+    public String getId() { return id; }
 
     public String getFirstName() {
         return firstName;
@@ -22,5 +40,14 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("firstName", firstName);
+        result.put("lastName", lastName);
+        result.put("email", email);
+        return result;
     }
 }
