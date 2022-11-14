36565f90299f72768ee0dc8910d576a881e19b98
Gerardo <gerardojuarez695@gmail.com>
2022-11-14 16:31:13 -0500

    Refactor HomeActivity

--- app/src/main/java/com/example/whatsappclone/HomeActivity.java
+++ app/src/main/java/com/example/whatsappclone/HomeActivity.java
@@ -11,13 +11,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -36,7 +34,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        ;
+
         initAuth();
         initViews();
 
@@ -77,8 +75,6 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         fabCreateMessage.setOnClickListener(view -> startActivity(new Intent(this, CreateMessageActivity.class)));
-
-
     }
 
     @Override
